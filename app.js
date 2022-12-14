889ea04ab33cdc8a7c55e0bd858e5747635f89f1
NaveenSankuri <husurpushpasankuri@gmail.com>
2022-12-14 18:31:42 +0530

    v.2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const mustacheExpress = require('mustache-express');
 port = 3000;
+
+
+//setting mustache engine
+app.engine('mustache', mustacheExpress());
+app.set('views', '/views');
+app.set('view engine', 'mustache');
+
 
 app.get('/', (req, res) => {
     res.send("Naveen");
